109c84bc59746be2c76e90a3e2ce3b3181a366ae
avivamsallem <49059393+avivamsallem@users.noreply.github.com>
2020-12-18 17:45:38 +0200

    first commit. added basic functionalities to database.py and backend.py

---
+++ backend.py
@@ -0,0 +1,107 @@
+from database import Database
+
+
+class Backend():
+
+    def __init__(self):
+        self.db = Database()
+
+    def login(self, username, password) -> tuple:
+
+        terms_dict = {'username': username, 'password': password}
+        results = self.db.select_query('Users', terms_dict)
+
+        if len(results) == 0:
+            return False, False
+
+        user_id = results[0][0]
+        # check if the user is an administrator
+        # query = f"SELECT * FROM Administrators WHERE uid='{user_id}'"
+
+        terms_dict_2 = {'uid': user_id}
+
+        results = self.db.select_query('Administrators', terms_dict_2)
+        is_admin = True if len(results) == 1 else False
+
+        if is_admin:
+            return True, True
+
+        else:
+            return True, False
+
+    def register(self, username, first_name, last_name, password, email, phone_number) -> bool:
+
+        terms_dict = {'username': username}
+
+        results = self.db.select_query('Users', terms_dict)
+
+        if len(results) == 1:
+            return False
+
+        values_dict = {'username': username,
+                       'firstName': first_name,
+                       'lastName': last_name,
+                       'password': password,
+                       'email': email,
+                       'phoneNumber': phone_number}
+
+        if not self.db.insert_query('Users', values_dict):
+            return False
+
+        return True
+
+    def add_new_ride(self, user_id: int, location: str, exit_time: str, exit_date: str, num_of_riders_capacity: list, cost: int, ride_kind: list, pick_up_places: list) -> bool:
+
+        values_dict = {'uid': user_id,
+                       'location': location,
+                       'exitTime': exit_time,
+                       'exitDate': exit_date,
+                       'numOfRiders': 0,
+                       'numOfRidersCapacity': num_of_riders_capacity,
+                       'cost': cost,
+                       'rideKind': ride_kind,
+                        'pickUpPlaces': pick_up_places}
+
+        if not self.db.insert_query('Rides', values_dict):
+            return False
+
+        return True
+
+    def join_ride(self, user_id_passenger: int, ride_id: int) -> tuple:
+        terms_dict = {'rid': ride_id}
+
+        rides_results = self.db.select_query('Rides', terms_dict)
+
+        if len(rides_results) != 1:
+            return False, "Couldn't find the requested ride. Please try again."
+
+        ride = rides_results[0]
+
+        riders_results = self.db.select_query('Riders', terms_dict)
+
+        for current_passenger in riders_results:
+            if current_passenger[1] == user_id_passenger:
+                return False, "You already assigned to this ride."
+
+        current_passengers_amount = ride[5]
+        passengers_capacity = ride[6]
+        if current_passengers_amount >= passengers_capacity:
+            return False, "This ride is already full. Please try another ride."
+
+        values_update_dict = {'numOfRiders': current_passengers_amount + 1}
+
+        if not self.db.update_query('Rides', values_update_dict, terms_dict):
+            return False, "Failed adding you to the ride. Please try again later."
+
+        terms_dict['uid'] = user_id_passenger
+
+        if not self.db.insert_query('Riders', terms_dict):
+            return False,  "Failed adding you to the ride. Please try again later."
+
+        return True, "You have been successfully joined the ride!"
+
+
+
+
+
+
---
+++ database.py
@@ -0,0 +1,99 @@
+import pyodbc
+
+
+class Database:
+    def __init__(self):
+        self.server = 'tcp:bgride-db.database.windows.net'
+        self.database = 'BGRIde'
+        self.username = 'aviv_alon'
+        self.password = 'Arnuyv123'
+        self.cursor = None
+        self.connection = None
+
+    def open_connection(self):
+        self.connection = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password)
+        self.cursor = self.connection.cursor()
+
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
+
+    def select_query(self, table, terms_dict=None) -> list:
+
+        query = f"SELECT * FROM {table}"
+
+        if terms_dict is not None:
+            terms_query = ' '
+            for term in terms_dict.keys():
+                terms_query = terms_query + f"{term}='{terms_dict[term]}' AND "
+            terms_query = terms_query[:-5]
+            query = f"{query} WHERE {terms_query}"
+
+        self.open_connection()
+
+        self.cursor.execute(query)
+
+        results = self.cursor.fetchall()
+
+        self.close_connection()
+
+        return results
+
+    def insert_query(self, table, values_dict) -> bool:
+        fields_query = "("
+        values_query = "("
+        for field in values_dict.keys():
+            fields_query = f"{fields_query}{field}, "
+            values_query = f"{values_query}'{values_dict[field]}', "
+
+        fields_query = f"{fields_query[:-2]}) "
+        values_query = f"{values_query[:-2]})"
+
+        query = f"INSERT INTO {table}{fields_query} VALUES{values_query}"
+
+        self.open_connection()
+
+        try:
+            self.cursor.execute(query)
+            self.cursor.commit()
+        except:
+            self.close_connection()
+            return False
+
+        if not self.cursor.rowcount != 0:
+            self.close_connection()
+            return False
+
+        self.close_connection()
+        return True
+
+    def update_query(self, table, values_update_dict: dict, terms_dict: dict) -> bool:
+
+        set_query = ''
+        terms_query = ''
+
+        for value in values_update_dict.keys():
+            set_query = f"{set_query}{value}='{values_update_dict[value]}', "
+
+        set_query = set_query[:-2]
+
+        for value in terms_dict.keys():
+            terms_query = f"{terms_query}{value}='{terms_dict[value]}' AND "
+
+        terms_query = terms_query[:-4]
+        query = f"UPDATE {table} SET {set_query} WHERE {terms_query}"
+        self.open_connection()
+
+        try:
+            self.cursor.execute(query)
+            self.cursor.commit()
+        except:
+            self.close_connection()
+            return False
+
+        self.close_connection()
+        return True
+
+
+
